06087b64dba7b88bb59b1f51e10b10ced77a6680
LeleValore <emanuelevalore@gmail.com>
2025-11-13 13:40:43 +0100

    aggiubnto main e stampa della matrice di traceback

--- es_allineamento_1.py
+++ es_allineamento_1.py
@@ -71,26 +71,65 @@ def trace_back(matrice, seq1, seq2, match, mismatch, gap):
     return aligne_sequence1, aligne_sequence2
 
 
+def print_traceback_matrice(matrice, seq1, seq2, match, mismatch, gap):
+    """" 
+    Stampa la matrice di trace back come un insimee di lettere dove:
+    D = Diagonale,
+    S = Sopra,
+    L = Sinistra
+    """
 
+    n = len(seq1) + 1
+    m = len(seq2) + 1 
+
+    for i in range(1,n):
+        row = []
+        for j in range(1,m):
+            current_score = matrice[i][j]
+            diagonal_score = matrice[i-1][j-1]
+            score_sopra = matrice[i-1][j]
+            score_sinistra = matrice[i][j-1]
+
+            if current_score == diagonal_score + (match if seq1[i-1] == seq2[j-1] else mismatch):
+                row.append("D")
+            elif current_score == score_sopra + gap:
+                row.append("S")
+            elif current_score == score_sinistra + gap:
+                row.appendd("L")
+            else:
+                row.append("0")
+
+        print("\t".join(row))
 
 
 
 
 def main():
-    seq1 = "AG"
-    seq2 = "ACG"
+
+    """Definire due sequenze T, S con lettere A, C, G, T"""
+
+    seq1 = "AGTAAT"
+    seq2 = "TGAAAT"
 
     match = 1 
     missmatch = -1 
     gap = -2 
 
     #1. Costruire la matrice di punteggio con stampa della matrice
-    allineamento = pairwise2.align.globalms(seq1, seq2, match, missmatch, gap, gap)
-    for a in allineamento:
-        print(format_alignment(*a))
+    matrice = build_matrice(seq1,seq2, match, missmatch, gap)
+    print("Matrice di punteggio:")
+    print_matrice(matrice)
 
     #2. Eseguire traceBack con stampa della matrice di traceBack
+    print("\nMatrice di trace back:")
+    print_traceback_matrice(matrice,seq1,seq2, match, missmatch,gap)
+    aligned_seq1, aligned_seq2 = trace_back(matrice, seq1, seq2, match, missmatch, gap)
 
-    #3. Fornire l'allineamnto ottimale
+    # 3. Allineamento Ottimale
+    print("\nAllineamento ottimale:")
+    print(aligned_seq1)
+    print(aligned_seq2)
+
+
 if __name__ == "__main__":
     main()  
